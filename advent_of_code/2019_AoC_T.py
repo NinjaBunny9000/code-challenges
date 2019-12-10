121b6c6c85e83f2187eb8791f2720acb684ff197
Bun <krissyheishman@gmail.com>
2019-12-09 17:31:45 -0800

    template and gitignore update

--- advent_of_code/2019_AoC_T.py
+++ advent_of_code/2019_AoC_T.py
@@ -1,10 +1,19 @@
+# Puzzle: 
+
 from datetime import datetime
 
 startTime = datetime.now()
 
+"""
+Puzzle Notes
+
+- 
+"""
+
 def solver():
     pass
 
+print(solver()) # sample test
 print(solver()) # the full test
 
 print(f"Run time: {datetime.now() - startTime}")
