1454ff5bf4a0c20b74232cd3a313f94700622151
Bun <ninjabunny9000@gmail.com>
2019-12-11 16:12:25 -0800

    codewars kata template

---
+++ code_wars/_template.py
@@ -0,0 +1,22 @@
+# Kata: 
+
+from datetime import datetime
+import pprint
+
+startTime = datetime.now()
+pp = pprint.PrettyPrinter()
+
+
+"""
+Notes
+
+- 
+"""
+
+def solver():
+    pass
+
+print(solver()) # sample test
+print(solver()) # the full test
+
+print(f"Run time: {datetime.now() - startTime}")
