19188607ab021711c90dfee4da6243b3fcb602ed
Bun <ninjabunny9000@gmail.com>
2019-12-12 18:08:26 -0800

    snail sort completed

--- code_wars/snail_sort.py
+++ code_wars/snail_sort.py
@@ -1,4 +1,4 @@
-# Kata: 
+# Kata: https://www.codewars.com/kata/snail/train/python
 
 from datetime import datetime
 import pprint
@@ -6,118 +6,87 @@ import pprint
 startTime = datetime.now()
 pp = pprint.PrettyPrinter()
 
-
 """
 Notes
 - given an nxn array, rearrange elements from outtermost to middle
 - traveling clockwise
+
+EXAMPLE 3x3: [1,2,3]
+             [4,5,6]
+             [7,8,9]
+
+SHOULD EQUAL: [1,2,3,6,9,8,7,4,5]
 """
 
-# new_list = [expression for member in iterable if variable condition]
-# new_list = [expression (if conditional) for member in iterable]
+def snail(array):
+    
+    # confirm n by n array
+    if isinstance(array[0], list) is False:
+        print("Array is not n by n.")
+        return 0
 
-test_array = [[1,2,3],[4,5,6],[7,8,9]]
-
-"""
-[1,2,3]
-[4,5,6]
-[7,8,9]
-
-traverse
-take the value of x,y
-right
-new_array.append(array[y+0][x+0])
-new_array.append(array[y+0][x+1])
-new_array.append(array[y+0][x+2])
-down
-new_array.append(array[y+1][x+2])
-new_array.append(array[y+2][x+2])
-left
-new_array.append(array[y+2][x+1])
-new_array.append(array[y+2][x+0])
-up
-new_array.append(array[y+1][x+0])
-right
-new_array.append(array[y+1][x+1])
-
-row/col
-right
-(0,0)
-(0,1)
-(0,2)
-
-down
-(1,2)
-(2,2)
-
-left
-(2,1)
-(2,0)
-
-up
-(1,0)
-(1,1)
-
-"""
-# TODO do this again but with removing the items from the original array
-def snail_sort(array):
-
-    new_array = []  # placeholder array
-
-    # get the length and height of the array
+    # get array dimms
     n = len(array[0])
+    print(f"Array is {n} by {n}:")
+    pp.pprint(array)  # print out the array we take in
+    sorted_array = []
+    iterations = 0  # track how many times around we go (for funsies)
 
-    # set up "cursor" positions
-    x = 0
-    y = 0
-    col = 0
-    row = 0
-    loop = 1
+    
+    while True:
 
+        # take off the top row (left to right)
+        print('\n1) L=>R')
+        if len(array) == 0:
+            break
+        # print("L->R")
+        for item in array[0]:
+            sorted_array.append(item)
+        array.pop(0)
+        pp.pprint(array)
+            
+        # take off everything on the ends (top to bottom)
+        if len(array) == 0:
+            break
+        print('\n2) U=>D')
+        for row in array:
+            sorted_array.append(row[-1])  # get last item on every row
+            number = row[-1]
+            array[array.index(row)].pop(-1)
+        pp.pprint(array)
 
-    while loop < n:
+        # take off the borrom row (right to left)
+        print('\n3) R=>L')
+        if len(array) == 0:
+            break
+        for i in range(len(array[0])-1, -1, -1):
+            sorted_array.append(array[-1][i])
+        array.pop(-1)
+        pp.pprint(array)
 
-        print(f"starting loop {loop} // {row=} {col=}")
+        # take the left (bottom to top)
+        print('\n4) D=>U')
+        if len(array) == 0:
+            break
+        for i in range(len(array)-1, -1, -1):
+            # print(f"{array[i][0]}")
+            sorted_array.append(array[i][0])
+            array[i].pop(0)
+        pp.pprint(array)
 
-        # right
-        for _ in range(n):
-            new_array.append(array[row][col])
-            col += 1
+        iterations += 1
 
-        # down
-        row = loop
-        col = n-loop
-        for _ in range(n-loop):
-            new_array.append(array[row][col])
-            row += 1
+    print(f"\nSorted in {iterations} iterations.\n")
 
-        # left
-        row = col
-        col = n-row
-        print(f"{row=}")
-        print(f"row={n-col}")
-        for _ in range(n-loop**loop):
-            new_array.append(array[row][col])
-            col -= 1
+    return sorted_array
 
-        # up
-        row = loop
-        col = loop-1
-        for _ in range(loop):
-            new_array.append(array[row][col])
+        
+# INPUT/VALIDATION DATA
+small_array = [[1,2,3],[4,5,6],[7,8,9]]
+seven_by_seven = [[150, 291, 554, 225, 184, 493, 958], [441, 523, 686, 669, 710, 266, 53], [88, 397, 160, 252, 605, 448, 88], [364, 761, 169, 883, 968, 212, 748], [891, 253, 967, 877, 764, 80, 164], [901, 691, 752, 269, 33, 847, 160], [230, 195, 198, 674, 697, 93, 121]]
 
-        # reset for next loop and right move
-        row = loop
-        col = loop-row
-        loop += 1
-
-
-
-
-    return new_array
-
-
-print(snail_sort(test_array)) # [1,2,3,6,9,8,7,4,5]
-# print(snail_sort()) # the full test
+# TESTS
+# print(snail(small_array)) # [1,2,3,6,9,8,7,4,5]
+print(snail(seven_by_seven)) # [150, 291, 554, 225, 184, 493, 958, 53, 88, 748, 164, 160, 121, 93, 697, 674, 198, 195, 230, 901, 891, 364, 88, 441, 523, 686, 669, 710, 266, 448, 212, 80, 847, 33, 269, 752, 691, 253, 761, 397, 160, 252, 605, 968, 764, 877, 967, 169, 883]
 
 print(f"Run time: {datetime.now() - startTime}")
